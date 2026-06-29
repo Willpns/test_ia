abdaeb497ddbd1b204369affa13636f481dbba3f
Willpns <pwill06am@gmail.com>
2026-06-29 13:37:41 +0200

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/test_ia",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
